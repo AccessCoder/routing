b0b61777df8a16bc895bba3a66564ebfc0309400
Dominic <andro5@web.de>
2022-09-28 15:12:43 +0200

    Conditional Rendering.Branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,50 @@
 import React, {useState} from 'react';
 import './App.css';
-import HomePage from "./Pages/HomePage";
-import {HashRouter, Route, Routes} from "react-router-dom";
-import ButtonPage from "./Pages/ButtonPage";
-import NavBar from "./Components/NavBar";
 
 function App() {
 
-    const[input, setInput] = useState("");
+    const [input, setInput] = useState("");
+    const [input2, setInput2] = useState("");
+    const [input3, setInput3] = useState("");
 
-  return (
-    <div className="App">
-        <h1> Fancy RouterPage</h1>
-      <HashRouter>
-          <NavBar/>
-          <p>Make a wish for a buttonName</p>
-          <input onChange={(e)=>setInput(e.target.value)}/>
-          <Routes>
-              <Route path={"/"} element={<HomePage/>}/>
-              <Route path={"/button"} element={<ButtonPage name={input}/>}/>
-          </Routes>
-      </HashRouter>
-    </div>
-  );
+    let hasInput:boolean = input.length > 0;
+    let hasInput2:boolean = input2.length > 0;
+    let hasInput3:boolean = input3.length > 0;
+
+    function condRendering(isVisible:boolean){
+        if (isVisible){
+            return <h1>We have an Input!</h1>
+        }else {
+            return <h1>Please insert something!</h1>
+        }
+    }
+
+    return (
+        <div className="App">
+            <h1> Fancy ConditionalRendering Page </h1>
+            <input onChange={(e) => setInput(e.target.value)}/>
+            <div>
+                {condRendering(hasInput)}
+            </div>
+            <input onChange={(e) => setInput2(e.target.value)}/>
+
+            {
+                hasInput2 ?
+                    <h1>We have an Input!</h1>
+                    :
+                    <h1>Please insert something!</h1>
+            }
+
+            <input onChange={(e) => setInput3(e.target.value)}/>
+            {
+                hasInput3 &&
+                <h1>We have an Input!</h1>
+                ||
+                <h1>Test</h1>
+            }
+
+        </div>
+    );
 }
 
 export default App;
